5f5382246c52748a36779d1cc22e519944be6ad4
Dener Rodrigues <dl.rodrigues94@gmail.com>
2020-05-15 17:53:34 -0300

    Add year in card info url

--- fut/urls.py
+++ fut/urls.py
@@ -12,7 +12,7 @@ fun_captcha_public_key = rc['funCaptchaPublicKey']
 
 
 # remote config - should be refresh every x seconds
-rc = requests.get('https://www.easports.com/fifa/ultimate-team/web-app/content/7D49A6B1-760B-4491-B10C-167FBC81D58A/2019/fut/config/companion/remoteConfig.json').json()
+rc = requests.get('https://www.easports.com/fifa/ultimate-team/web-app/content/20C1B296-B15C-4F72-AF0F-882F187EC2C9/2020/fut/config/companion/remoteConfig.json').json()
 
 if rc['pin'] != {"b": True, "bf": 500, "bs": 10, "e": True, "r": 3, "rf": 300}:
     print('>>> WARNING: ping variables changed: %s' % rc['pin'])
@@ -29,6 +29,6 @@ itemsPerPage['club'] = 91  # ea, please don't troll us :-)
 # TODO: read sku's from https://utas.mob.v1.fut.ea.com/ut/shards/v2
 
 # TODO: card info url not found yet
-card_info_url = 'https://fifa19.content.easports.com/fifa/fltOnlineAssets/7D49A6B1-760B-4491-B10C-167FBC81D58A/2019/fut/items/web/'  # TODO: get hash from somewhere, dynamic year
+card_info_url = 'https://www.easports.com/fifa/ultimate-team/web-app/content/20C1B296-B15C-4F72-AF0F-882F187EC2C9/{year}/fut/items/web/'  # TODO: get hash from somewhere, dynamic year
 # TODO: could be nice to add locals on startup
 messages_url = 'https://www.easports.com/fifa/ultimate-team/web-app/loc/en_US.json'  # TODO: needs to base64 decoded.
